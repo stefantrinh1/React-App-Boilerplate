5a13858b2953426ae2bd529c38918f2204fd6fb7
Stefan Trinh <stefantrinh1.work@gmail.com>
2019-03-29 00:19:40 +0000

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React Application</h1>
+        <p>
+          This is a boiler plate for a react application. it is custom made and
+          is not created with create-react-app command and has the bare
+          essentials to get your application working
+        </p>
+        <br />
+        <p>By Stefan Trinh</p>
+        <p>
+          <a href="http://www.stefantrinh.co.uk">www.stefantrinh.co.uk</a>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
